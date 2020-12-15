be4c1067df2f11592a688bf9b620f91bdb460b6f
Alexandre Reis <alexandre.reis@edubox.pt>
2020-12-15 10:43:49 +0000

    fix: add router

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
+const router = express.Router()
 const port = process.env.PORT || 3000
 
 // Define paths for Express config
@@ -24,94 +25,8 @@ app.use(express.static(publicDirectoryPath))
  * new routes
  * todo: convert this routes for a router with controllers and models structure
  */
-
-app.get('/login', (req, res) => {
-    res.render('login', {})
-});
-
-app.get('/register', (req, res) => {
-    res.render('register', {})
-})
-
-app.get('/forgot-password', (req, res) => {
-    res.render('forgotpassword', {})
-})
-
-app.get('', (req, res) => {
-    res.render('dashboard', {})
-})
-
-
-
-app.get('/about', (req, res) => {
-    res.render('about', {
-        title: 'About Me',
-        name: 'Andrew Mead'
-    })
-})
-
-app.get('/help', (req, res) => {
-    res.render('help', {
-        helpText: 'This is some helpful text.',
-        title: 'Help',
-        name: 'Andrew Mead'
-    })
-})
-
-app.get('/weather', (req, res) => {
-    if (!req.query.address) {
-        return res.send({
-            error: 'You must provide an address!'
-        })
-    }
-
-    geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
-        if (error) {
-            return res.send({ error })
-        }
-
-        forecast(latitude, longitude, (error, forecastData) => {
-            if (error) {
-                return res.send({ error })
-            }
-
-            res.send({
-                forecast: forecastData,
-                location,
-                address: req.query.address
-            })
-        })
-    })
-})
-
-app.get('/products', (req, res) => {
-    if (!req.query.search) {
-        return res.send({
-            error: 'You must provide a search term'
-        })
-    }
-
-    console.log(req.query.search)
-    res.send({
-        products: []
-    })
-})
-
-app.get('/help/*', (req, res) => {
-    res.render('404', {
-        title: '404',
-        name: 'Andrew Mead',
-        errorMessage: 'Help article not found.'
-    })
-})
-
-app.get('*', (req, res) => {
-    res.render('404', {
-        title: '404',
-        name: 'Andrew Mead',
-        errorMessage: 'Page not found.'
-    })
-})
+require("./routes/web.routes")(router)
+app.use("", router)
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
---
+++ src/routes/web.routes.js
@@ -0,0 +1,6 @@
+module.exports = (router) => {
+    router.get('/', (req, res) => { res.render('dashboard', {}) })
+    router.get('/login', (req, res) => { res.render('login', {}) })
+    router.get('/register', (req, res) => { res.render('register', {}) })
+    router.get('/forgot-password', (req, res) => { res.render('forgotpassword', {}) })
+}

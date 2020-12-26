239488bc0eb4f2c06b40ff783ea98d8ed73ac146
adevr <alex.cst.reis@gmail.com>
2020-12-26 00:19:01 +0000

    feat: method prototypes

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,36 @@
+const User = require("../models/index").User;
+
+module.exports = {
+    store: async (req, res, next) => {
+        let {email, picture, fullname, password} = req.body
+        if (email !== null) {
+            res.render("register", {
+                error: true,
+                message: "Invalid E-mail"
+            })
+        }
+
+        //todo: make validator middleware
+        const [user, created] = await User.findOrCreate({where: {email: email}, defaults: req.body})
+
+        if (created) {
+            // todo: send mail
+        }
+
+        if (user) {
+            res.render("register", {
+                error: true,
+                message: "User already registered on the platform"
+            })
+        }
+    },
+
+    update: async(req,res,next) => {
+        //todo: implement update user here
+    },
+
+    resetPass: async(req, res, next) => {
+        //todo: implement mailer here
+    },
+
+}
--- src/routes/web.routes.js
+++ src/routes/web.routes.js
@@ -1,10 +1,25 @@
 const TaskController = require("../controllers/task.controller")
+const UserController = require("../controllers/user.controller")
 const asyncRoute = require("../middleware/async.route")
 
 module.exports = (router) => {
-    router.get('/',  asyncRoute(TaskController.index))
+    router.get('/', asyncRoute(TaskController.index))
+
+    /**
+     * login and register routes
+     */
     router.get('/login', (req, res) => { res.render('login', {}) })
+    router.post('/login', (req, res) => { asyncRoute(UserController.store) })
     router.get('/register', (req, res) => { res.render('register', {}) })
     router.get('/forgot-password', (req, res) => { res.render('forgotpassword', {}) })
+
+    /***
+     * Tasks routes
+     */
     router.get('/tasks', asyncRoute(TaskController.index))
+
+    /**
+     * User routes
+     */
+
 }

93744e2c1974b95877058f42e7a85e1614042ce2
Alexandre Reis <alexandre.reis@edubox.pt>
2020-12-30 17:30:19 +0000

    chore: remove console.log

--- src/middleware/redirect.middleware.js
+++ src/middleware/redirect.middleware.js
@@ -1,6 +1,5 @@
 module.exports = {
     signInChecker: (req, res, next) => {
-        console.log(req.session.loggedin)
         if (req.session.loggedin === undefined && !req.session.loggedin) {
             res.redirect('/login')
         } else {

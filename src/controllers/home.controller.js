e292092cf6f20af092d8b37b57ee883d2e56e2e4
Alexandre Reis <alex.cst.reis@gmail.com>
2021-01-04 21:51:36 +0000

    fix: user with no tasks

--- src/controllers/home.controller.js
+++ src/controllers/home.controller.js
@@ -4,31 +4,34 @@ module.exports = {
         let today = new Date().getDay()
         const user = await User.findOne({
             where: {
-                email: req.session.email, 
+                email: req.session.email,
                 is_active: 1,
             },
-            include: {model: Task, where: {dayweek: today, deleted_at: null}},
+            include: {model: Task, where: {dayweek: today, deleted_at: null}, required:false},
             order: [['Tasks', "status", "DESC"], ['created_at', 'ASC']]
         })
-        
-        const tags = await Tag.findAll()
+
+        let tags = await Tag.findAll()
         let completedPercentage = [0, 0]
         let tagsCount = {}
-        
-        user.Tasks.forEach( task  => {
-            task.status === 1 ? completedPercentage[1]++ : completedPercentage[0]++
 
-            let newtags = [];
-            let tasktags = task.tags.split(",")
-            task.tags = [];
-            for (let i = 0; i < tasktags.length; i++) {
-                const id = tasktags[i];
-                newtags[i] = tags.filter(tag => tag.id == id);
-                (! tagsCount[id]) ? tagsCount[id] = 1 : tagsCount[id]++
-            }
-            task.tags.push(newtags);
-        })        
-
+        if (user.Tasks === null || user.Tasks === undefined || user.Tasks.length === 0) {
+            user.Tasks = null
+            tags = null
+        }else{
+            user.Tasks.forEach(task => {
+                task.status === 1 ? completedPercentage[1]++ : completedPercentage[0]++
+                let newtags = [];
+                let tasktags = task.tags.split(",")
+                task.tags = [];
+                for (let i = 0; i < tasktags.length; i++) {
+                    const id = tasktags[i];
+                    newtags[i] = tags.filter(tag => tag.id == id);
+                    (!tagsCount[id]) ? tagsCount[id] = 1 : tagsCount[id]++
+                }
+                task.tags.push(newtags);
+            })
+        }
 
         res.render("dashboard", {
             title: "Dashboard",

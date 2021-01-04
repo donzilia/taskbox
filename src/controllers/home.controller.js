87df5d653ed22b078fc155eb64331ae16c061e1b
Alexandre Reis <alexandre.reis@edubox.pt>
2021-01-04 11:45:35 +0000

    feat: tags count

--- src/controllers/home.controller.js
+++ src/controllers/home.controller.js
@@ -1,6 +1,5 @@
 const {Tag, User, Task} = require("../models/index")
 module.exports = {
-    
     index: async (req, res, next) => {
         let today = new Date().getDay()
         const user = await User.findOne({
@@ -13,9 +12,10 @@ module.exports = {
         })
         
         const tags = await Tag.findAll()
-        //todo: get daily tasks completation
         //todo: get all tags count (define algorithm)
-        let completedPercentage = [0, 1];
+        let completedPercentage = [0, 1]
+        let tagsCount = {}
+        
         user.Tasks.forEach( task  => {
             task.status === 1 ? completedPercentage[1]++ : completedPercentage[0]++
 
@@ -25,14 +25,18 @@ module.exports = {
             for (let i = 0; i < tasktags.length; i++) {
                 const id = tasktags[i];
                 newtags[i] = tags.filter(tag => tag.id == id);
+                (! tagsCount[id]) ? tagsCount[id] = 1 : tagsCount[id]++
             }
             task.tags.push(newtags);
         })        
 
+
         res.render("dashboard", {
             title: "Dashboard",
             user: user,
-            completedData: completedPercentage
+            completedData: completedPercentage,
+            tCounter: tagsCount,
+            tags: tags
         })
-    },
+    }
 };

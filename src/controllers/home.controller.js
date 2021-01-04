ac79a1f2785c77a3d5d506ed47e2f6f002d2dac2
Alexandre Reis <alexandre.reis@edubox.pt>
2021-01-04 11:56:19 +0000

    chore: cleanup code

--- src/controllers/home.controller.js
+++ src/controllers/home.controller.js
@@ -3,33 +3,23 @@ module.exports = {
     index: async (req, res, next) => {
         let today = new Date().getDay()
         const user = await User.findOne({
-            where: {
-                email: req.session.email, 
-                is_active: 1,
-            },
+            where: { email: req.session.email, is_active: 1},
             include: {model: Task, where: {dayweek: today, deleted_at: null}},
             order: [['Tasks', "status", "DESC"], ['created_at', 'ASC']]
         })
         
         const tags = await Tag.findAll()
-        //todo: get all tags count (define algorithm)
-        let completedPercentage = [0, 0]
-        let tagsCount = {}
-        
+        let completedPercentage = [0, 0], tagsCount = {}
         user.Tasks.forEach( task  => {
             task.status === 1 ? completedPercentage[1]++ : completedPercentage[0]++
-
-            let newtags = [];
-            let tasktags = task.tags.split(",")
+            let newtags = [] , tasktags = task.tags.split(",")
             task.tags = [];
             for (let i = 0; i < tasktags.length; i++) {
-                const id = tasktags[i];
-                newtags[i] = tags.filter(tag => tag.id == id);
-                (! tagsCount[id]) ? tagsCount[id] = 1 : tagsCount[id]++
+                newtags[i] = tags.filter(tag => tag.id == tasktags[i])
+                (! tagsCount[tasktags[i]]) ? tagsCount[tasktags[i]] = 1 : tagsCount[tasktags[i]]++
             }
             task.tags.push(newtags);
         })        
-
 
         res.render("dashboard", {
             title: "Dashboard",
